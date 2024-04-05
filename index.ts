c0fc1ba8bccc04552e62ed5fcb5b8c11d7a41503
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-04-05 10:59:21 +0500

    update home work task

--- index.ts
+++ index.ts
@@ -1,24 +1,69 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from 'chalk';
 
-let myTodo =[];
+let myTodo :string[]=[];
+
 let condition=true;
-while(condition){
-let todoTask = await inquirer.prompt(
-    [{
-        name:"todo",
-        type:"input",
-        message:"what do you want to add in your todos list?",
-},
+while(condition)
 {
+let ans = await inquirer.prompt(
+    [{
+        name:"select",
+        type:"list",
+        message:"what do you want to do in your todos list?",
+        choices:["add","view","update","delete"],
+},]);
+
+if(ans.select == "add"){
+    let addTodo=await inquirer.prompt({
+        name:"additem",
+        type:"input",
+        message:"please add in your todos",
+    });
+    myTodo.push(addTodo.additem)
+    console.log(chalk.blue.underline("-->",myTodo))
+
+}
+if(ans.select == "view"){
+    console.log(chalk.underline.yellow("--> ",myTodo))
+}
+if(ans.select == "update"){
+    let updateTodo=await inquirer.prompt({
+        name:"update",
+        type:"list",
+        message:"please select to update in your todos",
+        choices:myTodo.map(item => item),
+    });
+    let addTodo=await inquirer.prompt({
+        name:"additem",
+        type:"input",
+        message:"add todo",
+    });
+    let newtodo=myTodo.filter(val => val != updateTodo.update)
+    myTodo=[...newtodo,addTodo.additem]
+    console.log(chalk.underline.green("--> ",myTodo))
+
+}
+if(ans.select == "delete"){
+    let deleteTodo=await inquirer.prompt({
+        name:"delete",
+        type:"list",
+        message:"please select to delete in your todos",
+        choices:myTodo.map(item => item)
+    });
+    let newtodo=myTodo.filter(val => val != deleteTodo.delete)
+    myTodo=[...newtodo]
+    console.log(chalk.underline.redBright("--> ",myTodo))
+}
+let addMore=await inquirer.prompt({
     name:"addMore",
     type:"confirm",
     message:"Do you want to add more task?",
-    default:"false",
-}
-]);
+    default:"true",
+    });
 
-myTodo.push(todoTask.todo);
-condition = todoTask.addMore;
-console.log(myTodo);
+// //loop is running on the based of this variable condition
+condition = addMore.addMore;
 };
+
